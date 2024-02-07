780c4fcd761a55d53b87e522523619e51764b0ad
Meriem1551 <meriem200409@gmail.com>
2024-02-07 18:36:01 +0100

    a menu of operations

---
+++ data.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int data_menu()
+{
+    int choice;
+    printf("WELCOME TO OUR DATA BASE :DD\n");
+    printf("Choose one of the following recomandations :\n");
+    printf("\t1 - Add a new student.\n");
+    printf("\t2 - Search for a specific student by group.\n");
+    printf("\t3 - Display all students data.\n");
+    printf("\t3 - Sort all students.\n");
+    printf("\t4 - Delete a student.\n");
+    printf("\t0 - to exit.\n");
+    scanf("%d", &choice);
+    return choice;
+}

29860f0f3f891e5eb9eff2eac48b2e418e3f1144
Meriem1551 <meriem200409@gmail.com>
2024-03-03 22:05:23 +0100

    transfer if to switch

--- main.c
+++ main.c
@@ -10,20 +10,26 @@ int main(void)
     do
     {
         choice = data_menu();
-        if (choice == 1)
-            add_data(students, s);
-        if (choice == 2)
+        switch (choice)
         {
+        case 1:
+            add_data(students, s);
+            break;
+        case 2:
             printf("Enter the First name of the student you're looking for ");
             scanf("%s", &fn);
             found = search_by_fname(students, fn);
             printf("%d\n", found);
-        }
-        if (choice == 3)
+            break;
+        case 3:
             show_all_data(students);
-        //  if (choice == 4)
-        //      sort_data();
-        //  if (choice == 5)
-        //      delete_data();
+            break;
+        case 4:
+            // sort_data();
+            break;
+        case 5:
+            // delete_data();
+            break;
+        }
     } while (choice != 0);
 }
